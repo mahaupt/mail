ce83183c59f129cb40fb1b72e2063dca742b5816
mahaupt <subcortexx@gmail.com>
2025-07-16 16:20:57 +0200

    fix permissions

--- opendkim/run.sh
+++ opendkim/run.sh
@@ -6,3 +6,6 @@ envsubst < /etc/opendkim/template/opendkim.conf > /etc/opendkim/opendkim.conf
 envsubst < /etc/opendkim/template/KeyTable > /etc/opendkim/KeyTable
 envsubst < /etc/opendkim/template/SigningTable > /etc/opendkim/SigningTable
 envsubst < /etc/opendkim/template/TrustedHosts > /etc/opendkim/TrustedHosts
+
+chown opendkim:opendkim /etc/opendkim/keys/mail.private
+chmod 600 /etc/opendkim/keys/mail.private

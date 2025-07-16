367ffc2d991a75cd4ed9de066cb997d6ab834c01
mahaupt <subcortexx@gmail.com>
2025-07-16 14:50:24 +0200

    fix opendkim

--- opendkim/docker-entrypoint.sh
+++ opendkim/docker-entrypoint.sh
@@ -3,4 +3,6 @@ envsubst < /etc/opendkim/template/opendkim.conf > /etc/opendkim/opendkim.conf
 envsubst < /etc/opendkim/template/KeyTable > /etc/opendkim/KeyTable
 envsubst < /etc/opendkim/template/SigningTable > /etc/opendkim/SigningTable
 envsubst < /etc/opendkim/template/TrustedHosts > /etc/opendkim/TrustedHosts
-exec /usr/sbin/opendkim -f
+
+echo ">>> Starte OpenDKIM..."
+exec /usr/sbin/opendkim -f -x /etc/opendkim/opendkim.conf
